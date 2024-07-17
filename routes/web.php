fa8abef3192bb4410732cf05e23e99d1e101884e
AndryAditama <andryaditama@gmail.com>
2024-07-17 15:34:48 +0700

    make view (blade templating & layouting)

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-   return view('home', ['name' => 'John Doe']);
+   return view('home', ['title' => 'Home Page'], ['name' => 'John Doe']);
 });
 
 Route::get('/news', function () {
-   return view('news');
+   return view('news', ['title' => 'News Page']);
 });
 
 Route::get('/category', function () {
-   return view('category');
+   return view('category', ['title' => 'Category Page']);
 });
